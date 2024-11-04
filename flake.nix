f3c2919934a84c302782989e7535e2f576e76ed6
Christina Sørensen <christina@cafkafk.com>
2024-11-04 09:10:16 +0100

    build(flake): include cargo-outdated in devShell
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- flake.nix
+++ flake.nix
@@ -160,7 +160,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            pkgs.cargo-outdated
           ];
         };
       }
